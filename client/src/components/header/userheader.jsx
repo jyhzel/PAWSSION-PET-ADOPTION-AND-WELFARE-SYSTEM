d55b7ea57a302e68fa7585c69929ea919ce54a65
jyhzel <jhyzzeeld@gmail.com>
2026-06-05 13:59:31 +0800

    patname header

--- client/src/components/header/userheader.jsx
+++ client/src/components/header/userheader.jsx
@@ -1,31 +1,36 @@
+import { useLocation } from "react-router-dom";
 import { FaBell, FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <div className="w-full bg-[#f5f5f5] border border-gray-200 rounded-md px-6 py-4 flex items-center justify-between shadow-sm">
 
       {/* LEFT SIDE */}
       <h1 className="text-[44px] font-bold text-blue-700 tracking-tight">
-        Dashboard
+
+        {location.pathname === "/" && "Dashboard"}
+        {location.pathname === "/matchmaker" && "Pet Matchmaker"}
+        {location.pathname === "/adoption" && "Adoption Hub"}
+        {location.pathname === "/application" && "My Application"}
+        {location.pathname === "/donation" && "Donation"}
+        {location.pathname === "/kamustahan" && "Kamustahan"}
+
       </h1>
 
       {/* RIGHT SIDE */}
       <div className="flex items-center gap-8">
 
-        {/* BELL */}
         <button className="text-[32px] text-blue-900 hover:scale-105 transition">
           <FaBell />
         </button>
 
-        {/* PROFILE BUTTON */}
         <button className="flex items-center gap-3 border border-blue-700 rounded-full px-6 py-2 bg-white shadow-sm hover:bg-blue-50 transition">
-
           <FaUserCircle className="text-[34px] text-gray-500" />
-
           <span className="text-[28px] text-blue-900 font-medium">
             Profile
           </span>
-
         </button>
 
       </div>
